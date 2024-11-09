8134f55c2fd804dc3a34455e2e6027dcf5909ebf
Matilde Silva <114365956+whtifigo@users.noreply.github.com>
2024-11-09 14:38:07 +0000

    Added printf 🚀

---
+++ printf/ft_printf_str.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_str.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matda-co <matda-co@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/09 12:23:25 by matda-co          #+#    #+#             */
+/*   Updated: 2024/11/09 12:29:15 by matda-co         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft.h"
+#include "ft_printf.h"
+
+int	ft_printf_str(char *s)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+	{
+		ft_putstr_fd("(null)", 1);
+		return (6);
+	}
+	while (s[i])
+		i++;
+	ft_putstr_fd(s, 1);
+	return (i);
+}
---
+++ printf/libft/ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: matda-co <matda-co@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/28 16:04:10 by matda-co          #+#    #+#             */
+/*   Updated: 2024/10/28 16:22:08 by matda-co         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t			i;
+	unsigned char	*str;
+
+	i = 0;
+	str = (unsigned char *) s;
+	while (i < n)
+	{
+		if (str[i] == (unsigned char)c)
+			return ((void *) s + i);
+		i++;
+	}
+	return (NULL);
+}
