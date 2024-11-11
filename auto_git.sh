50369bd7bb1bde4991a974fc3f24f3e49b196796
Matilde Da Costa Silva <matda-co@c1r16s6.42porto.com>
2024-11-11 10:21:05 +0000

    ✨ auto commit ✨ on: 2024-11-11 10:21:05

--- auto_git.sh
+++ auto_git.sh
@@ -6,7 +6,7 @@ git add .
 
 git commit -m "✨ auto commit ✨ on: $(date +"%Y-%m-%d %H:%M:%S")"
 
-git push origin 42_printf
+git push origin HEAD
 
 echo "Pushed changes at $(date)" >> /home/matda-co/Documents/CoCo/42_printf/auto_git.log
 
